1e192774a9a506294035b8d672a7c7aa44bf8d29
SvetlanaBSA <shveta.ne@tut.by>
2023-06-05 21:46:34 +0300

    task 3_v2. FileUtils added

--- src/main/java/task3_v2/BookList.java
+++ src/main/java/task3_v2/BookList.java
@@ -1,23 +1,17 @@
 package task3_v2;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
 
 public class BookList {
-    private static List<Book> books;
+    private List<Book> books;
 
     public BookList() {
         this.books = new ArrayList<>();
     }
 
     public void getBookList() {
-        books = getBookListFromCSV();
+        getBookListFromCSV();
     }
 
     public void showAllBooksInTheBookList() {
@@ -54,42 +48,12 @@ public class BookList {
         System.out.println();
     }
 
-    private List<Book> getBookListFromCSV() {
+    private void getBookListFromCSV() {
         File inputFile = new File(Objects.requireNonNull(this.getClass().getResource("/testDataBookList.csv")).getFile());
         String filePath = inputFile.getAbsolutePath();
 
-        books = readBooksFromCSV(filePath);
-        return books;
-    }
-
-    private List<Book> readBooksFromCSV(String fileName) {
-        List<Book> books = new ArrayList<>();
-        Path pathToFile = Paths.get(fileName);
-        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
-            String line = br.readLine();
-            while (line != null) {
-                String[] bookLine = line.split(",");
-                Book book = createBook(bookLine);
-                books.add(book);
-                line = br.readLine();
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-        return books;
-    }
-
-    private Book createBook(String[] metadata) {
-        Book newBook = new Book.Builder()
-                .withId(Integer.parseInt(metadata[0])) //id
-                .withTitle(metadata[1]) //title
-                .withAuthors(metadata[2]) //authors
-                .withYear(Integer.parseInt(metadata[3])) //year
-                .withPagesCount(Integer.parseInt(metadata[4])) //pagesCount
-                .withPrice(Integer.parseInt(metadata[5])) //price
-                .withType(metadata[6]) //type
-                .build();
-        return(newBook);
+        FileUtils fileUtils = new FileUtils();
+        books = fileUtils.readBooksFromCSV(filePath);
     }
 
     private void printBooks(List<Book> books) {
---
+++ src/main/java/task3_v2/FileUtils.java
@@ -0,0 +1,43 @@
+package task3_v2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtils {
+
+    public List<Book> readBooksFromCSV(String fileName) {
+        List<Book> books = new ArrayList<>();
+        Path pathToFile = Paths.get(fileName);
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] bookLine = line.split(",");
+                Book book = createBook(bookLine);
+                books.add(book);
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return books;
+    }
+
+    private Book createBook(String[] metadata) {
+        Book newBook = new Book.Builder()
+                .withId(Integer.parseInt(metadata[0])) //id
+                .withTitle(metadata[1]) //title
+                .withAuthors(metadata[2]) //authors
+                .withYear(Integer.parseInt(metadata[3])) //year
+                .withPagesCount(Integer.parseInt(metadata[4])) //pagesCount
+                .withPrice(Integer.parseInt(metadata[5])) //price
+                .withType(metadata[6]) //type
+                .build();
+        return newBook;
+    }
+}
